68c21476a35a97926650beb0c095b8ce83b1f3cb
MikeHorn-git <123373126+MikeHorn-git@users.noreply.github.com>
2024-01-06 18:30:50 +0000

    Optimize Script

--- WAFS.ps1
+++ WAFS.ps1
@@ -23,6 +23,7 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE	#
 #SOFTWARE.                                                                      #
 #################################################################################
+
 [CmdletBinding()]
 param (
     [switch]$all,
@@ -34,151 +35,118 @@ function anti {
 
     Write-Output '[+] Anti-Forensics Script' -foregroundcolor "DarkGray"
 
-    # Disable Audit Success logs
-    auditpol /set /subcategory:"Filtering Platform Connection" /success:disable /failure:enable 2>$null
+    $PathsToRemove = @{
+        'ChromeCache'           = "$Home\AppData\Local\Google\Chrome\User Data\Default\Cache"                                   # Clean Chrome cache
+        'ChromeHistory'         = "$Home\AppData\Local\Google\Chrome\User Data\Default\History"                                 # Clean Chrome history
+        'ChromeSessionRestore'  = "$Home\AppData\Local\Google\Chrome\User Data\Default"                                         # Clean Chrome Session Restore
+        'EdgeCache'             = "$Home\AppData\Local\Packages\microsoft.microsoftedge_*\AC\MicrosoftEdge\Cache"               # Clean Edge cache
+        'IEHistory'             = 'HKCU:\Software\Microsoft\Internet Explorer\TypedURLs'                                        # Clean Edge / Internet Explorer history
+        'IEWebCache'            = "$Home\AppData\Local\Microsoft\Windows\WebCache\WebCacheV*.dat"                               # Clean Edge / Internet Explorer history
+        'FirefoxCache'          = "$Home\AppData\Local\Mozilla\Firefox\Profiles\*.default\Cache"                                # Clean Firefox cache
+        'FirefoxHistory'        = "$Home\AppData\Roaming\Mozilla\Firefox\Profiles\*.default\places.sqlite"                      # Clean Firefox history
+        'FirefoxSessionRestore' = "$Home\AppData\Roaming\Mozilla\Firefox\Profiles\*.default\sessionstore.js"                    # Clean Firefox Session Restore
+        'IECache'               = "$Home\AppData\Local\Microsoft\Windows\INetCache\IE"                                          # Clean Internet Explorer cache
+        'IECacheStorage'        = "$Home\AppData\Local\Microsoft\Internet Explorer\CacheStorage"                                # Clean Internet Explorer cache
+        'IESessionRestore'      = "$Home\AppData\Local\Microsoft\Internet Explorer\Recovery"                                    # Clean Internet Explorer Session Restore
+        'LastVisitedMRU'        = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRU'        # Clean Last-Visited MRU
+        'OpenSaveMRU'           = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU'           # Clean OpenSaveMRU
+        'PlugAndPlayLogs'       = "C:\Windows\INF\setupapi.dev*\"                                                               # Clean Plug and Play Logs
+        'Prefetch'              = "C:\Windows\Prefetch"                                                                         # Clean Prefetch
+        'RecentItems'           = "$HOME\AppData\Roaming\Microsoft\Windows\Recent"                                              # Clean Recent Items
+        'RecentDocs'            = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs'                         # Clean Recent Items
+        'RunMRU'                = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU'                             # Clean Run Command history
+        'ShadowCopies'          = 'HKLM:\SYSTEM\CurrentControlSet\Control\BackupRestore\FilesNotToBackup'                       # Disable Shadow Copies
+        'ShellBags'             = 'HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell'                      # Clean ShellBags
+        'ShellNoRoam'           = 'HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\ShellNoRoam'                # Clean ShellBags
+        'ShellWow6432'          = 'HKCU:\Software\Classes\Wow6432Node\Local Settings\Software\Microsoft\Windows\Shell\'         # Clean ShellBags
+        'Simcache'              = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\AppCompatCache\'                      # Clean Simcache
+        'SRUDB'                 = "C:\Windows\System32\sru\SRUDB.dat"                                                           # Clean System Resource Usage Monitor database
+        'TempFiles'             = "C:\Windows\temp\*"                                                                           # Clean Temp files
+        'Thumbcache'            = "$Home\AppData\Local\Microsoft\Windows\Explorer\thumbcache*.db\"                              # Clean Thumbcache
+        'USBHistory'            = 'HKLM:\SYSTEM\CurrentControlSet\Enum\USBSTOR'                                                 # Clean USB history
+        'USBEnum'               = 'HKLM:\SYSTEM\CurrentControlSet\Enum\USB'                                                     # Clean USB history
+        'UserAssist'            = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\UserAssist\'                        # Clean UserAssist
+        'VPNCache'              = 'HKLM:\Microsoft\Windows NT\CurrentVersion\NetworkList\Nla\Cache'                             # Clean VPN cache
+        'TimelineDB'            = "$Home\AppData\Local\ConnectedDevicesPlatform\*\ActivitiesCache.db"                           # Clean Windows Timeline DB
+        'PowerShellHistory'     = "$HOME\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"       # Clean Powershell history
+    }
 
-    # Clean Chrome cache
-    Remove-Item -Path "$Home\AppData\Local\Google\Chrome\User Data\Default\Cache" -Recurse -Force 2>$null
+    foreach ($pathKey in $PathsToRemove.Keys) {
+        $path = $PathsToRemove[$pathKey]
+        if ($null -ne $path) {
+            try {
+                Remove-Item -Path $path -Recurse -Force -ErrorAction Stop
+                Write-Output "Removed: $path"
+            }
+            catch {
+                Write-Error "Failed to remove: $path. Error: $_"
+            }
+        }
+    }
 
-    # Clean Chrome history
-    Remove-Item -Path "$Home\AppData\Local\Google\Chrome\User Data\Default\History" -Recurse -Force 2>$null
+    try {
+        # Disable Audit Success logs
+        auditpol /set /subcategory:"Filtering Platform Connection" /success:disable /failure:enable 2>$null
 
-    # Clean Chrome Session Restore
-    Remove-Item -Path "$Home\AppData\Local\Google\Chrome\User Data\Default" -Recurse -Force 2>$null
+        # Remove Cortana
+        Get-AppxPackage -AllUsers Microsoft.549981C3F5F10 | Remove-AppPackage 2>$null
 
-    # Remove Cortana
-    Get-AppxPackage -AllUsers Microsoft.549981C3F5F10 | Remove-AppPackage 2>$null
+        # Clean DNS cache
+        ipconfig /flushdns >$null
 
-    # Clean DNS cache
-    ipconfig /flushdns >$null
+        # Disable Keylogger
+        Stop-Service -Name DiagTrack -Force 2>$null
+        Set-Service -Name DiagTrack -StartupType Disabled 2>$null
+        Stop-Service -Name dmwappushservice -Force 2>$null
+        Set-Service -Name dmwappushservice -StartupType Disabled 2>$null
+        Write-Output "" > C:\ProgramData\Microsoft\Diagnosis\ETLLogs\AutoLogger\AutoLogger-Diagtrack-Listener.etl 2>$null
 
-    # Clean Edge cache
-    Remove-Item -Path "$Home\AppData\Local\Packages\microsoft.microsoftedge_*\AC\MicrosoftEdge\Cache" -Recurse -Force 2>$null
+        # Disable NTFS Last Access Time
+        Get-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' | New-ItemProperty -Name 'NtfsDisableLastAccessUpdate' -Value "1" -PropertyType DWORD -Force >$null
+        fsutil behavior set disablelastaccess 3 >$null
 
-    # Clean Edge / Internet Explorer history
-    Remove-Item -Path 'HKCU:\Software\Microsoft\Internet Explorer\TypedURLs' -Recurse -Force 2>$null
-    Remove-Item -Path "$Home\AppData\Local\Microsoft\Windows\WebCache\WebCacheV*.dat" -Force 2>$null
+        # Disable Prefetch
+        Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\' | New-ItemProperty -Name 'EnablePrefetcher' -Value "0" -PropertyType DWORD -Force >$null
+        Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\' | New-ItemProperty -Name 'EnableSuperfetch' -Value "0" -PropertyType DWORD -Force >$null
 
-    # Clean Firefox cache
-    Remove-Item -Path "$Home\AppData\Local\Mozilla\Firefox\Profiles\*.default\Cache" -Recurse -Force 2>$null
+        # Clean RecycleBin
+        Clear-RecycleBin -Force 2>$null
 
-    # Clean Firefox history
-    Remove-Item -Path "$Home\AppData\Roaming\Mozilla\Firefox\Profiles\*.default\places.sqlite" -Force 2>$null
+        # Disable previous Shadow Copies
+        Get-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\' | New-ItemProperty -Name 'DisableLocalPage' -Value "1" -PropertyType DWORD -Force >$null
 
-    # Clean Firefox Session Restore
-    Remove-Item -Path "$Home\AppData\Roaming\Mozilla\Firefox\Profiles\*.default\sessionstore.js" -Force 2>$null
+        # Clean Shadow Copies
+        vssadmin delete shadows /All >$null
 
-    # Clean Internet Explorer cache
-    Remove-Item -Path "$Home\AppData\Local\Microsoft\Windows\INetCache\IE" -Recurse -Force 2>$null
-    Remove-Item -Path "$Home\AppData\Local\Microsoft\Internet Explorer\CacheStorage" -Recurse -Force 2>$null
+        # Disable ShellBags
+        Get-Item -Path 'HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell' | New-ItemProperty -Name 'BagMRU Size' -Value "1" -PropertyType DWORD -Force >$null
 
-    # Clean Internet Explorer Session Restore
-    Remove-Item -Path "$Home\AppData\Local\Microsoft\Internet Explorer\Recovery" -Recurse -Force 2>$null
+        # Disable UserAssist
+        Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\' | New-ItemProperty -Name 'Start_TrackProgs' -Value "0" -PropertyType DWORD -Force >$null
+        Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\' | New-ItemProperty -Name 'Start_TrackEnabled' -Value "0" -PropertyType DWORD -Force >$null
 
-    # Disable Keylogger
-    Stop-Service -Name DiagTrack -Force 2>$null
-    Set-Service -Name DiagTrack -StartupType Disabled 2>$null
-    Stop-Service -Name dmwappushservice -Force 2>$null
-    Set-Service -Name dmwappushservice -StartupType Disabled 2>$null
+        # Disable Windows Event logs
+        Stop-Service -Name EventLog -Force 2>$null
+        Set-Service EventLog -StartupType Disabled
 
-    Write-Output "" > C:\ProgramData\Microsoft\Diagnosis\ETLLogs\AutoLogger\AutoLogger-Diagtrack-Listener.etl 2>$null
+        # Clean Windows Event logs
+        wevtutil el | Foreach-Object { wevtutil cl "$_" } 2>$null
 
-    # Clean Last-Visited MRU
-    Remove-Item -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRU' -Recurse -Force 2>$null
+        # Clean Windows logs
+        Get-EventLog -LogName * | ForEach-Object { Clear-EventLog $_.Log } 2>$null
 
-    # Disable NTFS Last Access Time
-    Get-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' | New-ItemProperty -Name 'NtfsDisableLastAccessUpdate' -Value "1" -PropertyType DWORD -Force >$null
-    fsutil behavior set disablelastaccess 3 >$null
+        # Disable Windows Timeline DB
+        Stop-Service -Name CDPUserSvc* -Force 2>$null
 
-    # Clean OpenSaveMRU
-    Remove-Item -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU' -Recurse -Force 2>$null
+        # Disable $UsnJrnl
+        fsutil usn deletejournal /d c: 2>$null
 
-    # Clean Plug and Play Logs
-    Remove-Item -Path "C:\Windows\INF\setupapi.dev*\" -Force 2>$null
-
-    # Disable Prefetch
-    Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\' | New-ItemProperty -Name 'EnablePrefetcher' -Value "0" -PropertyType DWORD -Force >$null
-    Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\' | New-ItemProperty -Name 'EnableSuperfetch' -Value "0" -PropertyType DWORD -Force >$null
-
-    # Clean Prefetch
-    Remove-Item -Path "C:\Windows\Prefetch" -Recurse -Force 2>$null
-
-    # Clean Recent Items
-    Remove-Item -Path "$HOME\AppData\Roaming\Microsoft\Windows\Recent" -Recurse 2>$null
-    Remove-Item -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs' -Recurse -Force 2>$null
-
-    # Clean RecycleBin
-    Clear-RecycleBin -Force 2>$null
-
-    # Clean Run Command history
-    Remove-Item -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Recurse -Force 2>$null
-
-    # Disable Shadow Copies
-    Remove-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\BackupRestore\FilesNotToBackup' -Recurse -Force 2>$null
-
-    # Disable previous Shadow Copies
-    Get-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\' | New-ItemProperty -Name 'DisableLocalPage' -Value "1" -PropertyType DWORD -Force >$null
-
-    # Clean Shadow Copies
-    vssadmin delete shadows /All >$null
-
-    # Disable ShellBags
-    Get-Item -Path 'HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell' | New-ItemProperty -Name 'BagMRU Size' -Value "1" -PropertyType DWORD -Force >$null
-
-    # Clean ShellBags
-    Remove-Item -Path 'HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell' -Recurse -Force 2>$null
-    Remove-Item -Path 'HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\ShellNoRoam' -Recurse -Force 2>$null
-    Remove-Item -Path 'HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\' -Recurse -Force 2>$null
-    Remove-Item -Path 'HKCU:\Software\Classes\Wow6432Node\Local Settings\Software\Microsoft\Windows\Shell\' -Recurse -Force 2>$null
-
-    # Clean Simcache
-    Remove-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\AppCompatCache\' -Recurse -Force 2>$null
-
-    # Clean System Resource Usage Monitor database
-    Remove-Item -Path "C:\Windows\System32\sru\SRUDB.dat" -Force 2>$null
-
-    # Clean Temp files
-    Remove-Item -Path "C:\Windows\temp\*" -Recurse -Force 2>$null
-
-    # Clean Thumbcache
-    Remove-Item -Path "$Home\AppData\Local\Microsoft\Windows\Explorer\thumbcache*.db\" -Recurse -Force 2>$null
-
-    # Clean USB history
-    Remove-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Enum\USBSTOR' -Recurse -Force 2>$null
-    Remove-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Enum\USB' -Recurse -Force 2>$null
-
-    # Disable UserAssist
-    Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\' | New-ItemProperty -Name 'Start_TrackProgs' -Value "0" -PropertyType DWORD -Force >$null
-    Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\' | New-ItemProperty -Name 'Start_TrackEnabled' -Value "0" -PropertyType DWORD -Force >$null
-
-    # Clean UserAssist
-    Remove-Item -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\UserAssist\' -Recurse -Force 2>$null
-
-    # Clean VPN cache
-    Remove-Item -Path 'HKLM:\Microsoft\Windows NT\CurrentVersion\NetworkList\Nla\Cache' -Recurse -Force 2>$null
-
-    # Disable Windows Event logs
-    Stop-Service -Name EventLog -Force 2>$null
-    Set-Service EventLog -StartupType Disabled
-
-    # Clean Windows Event logs
-    wevtutil el | Foreach-Object {wevtutil cl "$_"} 2>$null
-
-    # Clean Windows logs
-    Get-EventLog -LogName * | ForEach-Object { Clear-EventLog $_.Log } 2>$null
-
-    # Disable Windows Timeline DB
-    Stop-Service -Name CDPUserSvc* -Force 2>$null
-
-    # Clean Windows Timeline DB
-    Remove-Item -Path "$Home\AppData\Local\ConnectedDevicesPlatform\*\ActivitiesCache.db" -Force
-
-    # Disable $UsnJrnl
-    fsutil usn deletejournal /d c: 2>$null
-
-    # Clean Powershell history
-    Remove-Item -Path "$HOME\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt" -Force 2>$null
-    Clear-History 2>$null
+        # Clean Powershell history
+        Clear-History 2>$null
+    }
+    catch {
+        Write-Error "An error occurred: $_"
+    }
 
     Write-Output '[+] Done, reboot your system' -foregroundcolor "DarkGray"
     Exit 0
@@ -192,31 +160,32 @@ function tools {
 
     # Download and install some programs
     $FastURL = [ordered]@{
-        DSPURL = "https://github.com/LloydLabs/delete-self-poc/releases/download/v1.1/ds_x64.exe"
-        ExifURL = "https://www.two-pilots.com/colorpilot.com/load/exif_64.exe"
+        DSPURL         = "https://github.com/LloydLabs/delete-self-poc/releases/download/v1.1/ds_x64.exe"
+        ExifURL        = "https://www.two-pilots.com/colorpilot.com/load/exif_64.exe"
         TimestomperURL = "https://github.com/slyd0g/TimeStomper/blob/master/Release/TimeStomper.exe"
         USBSentinelURL = "https://github.com/thereisnotime/xxUSBSentinel/releases/download/v1/xxUSBSentinel.exe"
-        VeracryptURL = "https://launchpad.net/veracrypt/trunk/1.25.9/+download/VeraCrypt%20Portable%201.25.9.exe"
+        VeracryptURL   = "https://launchpad.net/veracrypt/trunk/1.25.9/+download/VeraCrypt%20Portable%201.25.9.exe"
     }
 
     foreach ($key in $FastURL.Keys) {
         $url = $FastURL[$key]
         $outputPath = "$Home\Tools\$key"
 
-        Invoke-WebRequest -Uri $url -OutFile $outputPath -ErrorAction SilentlyContinue
-        if ($LASTEXITCODE -eq 0) {
+        try {
+            Invoke-WebRequest -Uri $url -OutFile $outputPath -ErrorAction Stop
             Write-Output "Downloaded $key from $url to $outputPath"
-        } else {
-            Write-Error "Failed to download $key from $url"
+        }
+        catch {
+            Write-Error "Failed to download $key from $url. $_"
         }
     }
 
 
     $LongURL = [ordered]@{
         BleachbitURL = "https://www.bleachbit.org/download/file/t?file=BleachBit-4.4.2-portable.zip"
-        BuskillURL = "https://github.com/BusKill/buskill-app/releases/download/v0.7.0/buskill-win-v0.7.0-x86_64.zip"
-        ClamavURL = "https://www.clamav.net/downloads/production/clamav-1.1.1.win.x64.zip"
-        SdeleteURL = "https://download.sysinternals.com/files/SDelete.zip"
+        BuskillURL   = "https://github.com/BusKill/buskill-app/releases/download/v0.7.0/buskill-win-v0.7.0-x86_64.zip"
+        ClamavURL    = "https://www.clamav.net/downloads/production/clamav-1.1.1.win.x64.zip"
+        SdeleteURL   = "https://download.sysinternals.com/files/SDelete.zip"
     }
 
     foreach ($key in $LongURL.Keys) {
@@ -224,13 +193,14 @@ function tools {
         $downloadPath = "$Home\Downloads\$key.zip"
         $extractPath = "$Home\Tools\$key"
 
-        Invoke-WebRequest -Uri $url -OutFile $downloadPath -ErrorAction SilentlyContinue
-        if ($LASTEXITCODE -eq 0) {
+        try {
+            Invoke-WebRequest -Uri $url -OutFile $downloadPath -ErrorAction Stop
             Write-Output "Downloaded $key from $url to $downloadPath"
             Expand-Archive -Path $downloadPath -DestinationPath $extractPath -Force
             Write-Output "Extracted $key to $extractPath"
-        } else {
-            Write-Error "Failed to download $key from $url"
+        }
+        catch {
+            Write-Error "Failed to download or extract $key from $url. $_"
         }
     }
 }
@@ -261,12 +231,12 @@ function main {
         anti
         Stop-Transcript
     }
-    elseif ($anti){
+    elseif ($anti) {
         Start-Transcript -Path ".\logs_anti.txt"
         anti
         Stop-Transcript
     }
-    elseif ($tools){
+    elseif ($tools) {
         Start-Transcript -Path ".\logs_tools.txt"
         tools
         Write-Output '[+] Done, reboot your system' -foregroundcolor "DarkGray"
